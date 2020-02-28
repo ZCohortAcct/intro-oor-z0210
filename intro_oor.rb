0873a94c1e84148ff582d917b221e085504fe590
NickisKnowledge <lifechangingstuff@gmail.com>
2020-02-27 20:57:20 -0600

    Code from THursday SG (02/27/20)

--- intro_oor.rb
+++ intro_oor.rb
@@ -5,28 +5,51 @@ class Toy
   # attr_writer :brand, :size, :color, :name, :cost
   # attr_reader :brand, :size, :color, :name, :cost
 
-  attr_accessor :brand, :size, :color, :name, :cost
+  attr_accessor :brand, :size, :color, :cost
+  @@all = []
 
   def initialize(title, price, hue, maker)
     @name = title
     @cost = price
     @color = hue
     @brand = maker
+
+    # binding.pry
+
+    puts self # refers to Toy obj/instance
+
+    @@all << self
+  end
+
+  def self.whats_self
+    puts self # refers to Toy class
+  end
+
+  def self.all # class reader (class method)
+    puts self # refers to Toy class
+    @@all
+  end
+
+  #getter / reader method ~> instance method
+  def name
+    puts self # refers to Toy obj/instance
+    @name
   end
 
   # setter / writer method
   # set an objs attributes 
-  # def name=(title) # instance method 
-  #   @name = title
-  # end
+  def name=(title) # instance method 
+    puts self # refers to Toy obj/instance
 
-  #getter / reader method
-  # def name
-  #   @name
-  # end
+    @name = title
+  end
 end
 
-ball = Toy.new('blocks', 30, 'multi-color', 'lego')
+lego_blocks = Toy.new('blocks', 30, 'multi-color', 'lego')
+# football = Toy.new('football', 50, 'brown & white', 'Nike')
+# minnion = Toy.new('minion figure', 14, 'yellow', 'Pic')
+# doll = Toy.new('LoL Doll', 7, 'orange', 'LoL Doll')
+
 # ball.name = 'football'
 # ball.cost = 40
 # ball.color = 'brown & white'
